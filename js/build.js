da9eda3526f1d6b3e3b783e02f2b7af6db0739cb
duyphuoc1990 <duyphuoc1990@gmail.com>
2015-03-26 09:53:31 +0900

    Update build.js

--- js/build.js
+++ js/build.js
@@ -1074,8 +1074,8 @@ AlertWord.prototype = {
     },
     notifBlink:function(id){
 	if(NOTIF_FLAG && $("#history-"+id)!=null && id==this.nextWord.id){
-		$("#history-"+id).find("div.icon-bell icon-bell").fadeOut(500);
-		$("#history-"+id).find("div.icon-bell icon-bell").fadeIn(500);
+		$("#history-"+id).find(".icon-bell icon-bell").fadeOut(500);
+		$("#history-"+id).find(".icon-bell icon-bell").fadeIn(500);
 		setTimeout(this.notifBlink.bind(this, id), 1000);
 		}
 	},
