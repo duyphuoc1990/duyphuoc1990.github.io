2bdef61211a9765a731e4290d761a4ae484c7af4
duyphuoc1990 <duyphuoc1990@gmail.com>
2015-03-26 10:13:38 +0900

    Update build.js

--- js/build.js
+++ js/build.js
@@ -701,7 +701,7 @@ wanakana = wanakana || {}, wanakana.version = "1.3.7", "function" == typeof defi
         return d = Math.min(h, g - e)
     }; g > e; ) {
         for (j(); d > 0; ) {
-            if (c = f(), wanakana.isKatakana(c) && (c = wanakana._katakanaToHiragana(c)), "っ" === c.charAt(0) && 1 === d && g - 1 > e) {
+            if (c = f(), wanakana.isKatakana(c) && (c = wanakana._katakanaToHiragana(c)), "Á" === c.charAt(0) && 1 === d && g - 1 > e) {
                 i = !0, l = "";
                 break
             }
@@ -727,22 +727,22 @@ wanakana = wanakana || {}, wanakana.version = "1.3.7", "function" == typeof defi
             else {
                 if ("n" === e.charAt(0)) {
                     if (b.IMEMode && "'" === e.charAt(1) && 2 === f) {
-                        k = "ん";
+                        k = "ñ";
                         break
                     }
                     wanakana._isCharConsonant(e.charAt(1), !1) && wanakana._isCharVowel(e.charAt(2)) && (f = 1, d = h(), e = d.toLowerCase())
                 }
-                "n" !== e.charAt(0) && wanakana._isCharConsonant(e.charAt(0)) && d.charAt(0) === d.charAt(1) && (f = 1, e = d = wanakana._isCharInRange(d.charAt(0), wanakana.UPPERCASE_START, wanakana.UPPERCASE_END) ? "ッ" : "っ")
+                "n" !== e.charAt(0) && wanakana._isCharConsonant(e.charAt(0)) && d.charAt(0) === d.charAt(1) && (f = 1, e = d = wanakana._isCharInRange(d.charAt(0), wanakana.UPPERCASE_START, wanakana.UPPERCASE_END) ? "Ģ" : "Á")
             }
             if (k = wanakana.R_to_J[e], null != k)
                 break;
             4 === f ? f -= 2 : f--
         }
-        null == k && (d = wanakana._convertPunctuation(d), k = d), (null != b ? b.useObseleteKana : void 0) && ("wi" === e && (k = "ゐ"), "we" === e && (k = "ゑ")), b.IMEMode && "n" === e.charAt(0) && ("y" === a.charAt(g + 1).toLowerCase() && wanakana._isCharVowel(a.charAt(g + 2)) === !1 || g === l - 1 || wanakana.isKana(a.charAt(g + 1))) && (k = d.charAt(0)), c || i(d.charAt(0)) && (k = wanakana._hiraganaToKatakana(k)), j.push(k), g += f || 1
+        null == k && (d = wanakana._convertPunctuation(d), k = d), (null != b ? b.useObseleteKana : void 0) && ("wi" === e && (k = "î"), "we" === e && (k = "ï")), b.IMEMode && "n" === e.charAt(0) && ("y" === a.charAt(g + 1).toLowerCase() && wanakana._isCharVowel(a.charAt(g + 2)) === !1 || g === l - 1 || wanakana.isKana(a.charAt(g + 1))) && (k = d.charAt(0)), c || i(d.charAt(0)) && (k = wanakana._hiraganaToKatakana(k)), j.push(k), g += f || 1
     }
     return j.join("")
 }, wanakana._convertPunctuation = function(a) {
-    return "　" === a ? " " : "-" === a ? "ー" : a
+    return "" === a ? " " : "-" === a ? "" : a
 }, wanakana.isHiragana = function(a) {
     var b;
     return b = a.split(""), b.every(wanakana._isCharHiragana)
@@ -767,7 +767,7 @@ wanakana = wanakana || {}, wanakana.version = "1.3.7", "function" == typeof defi
     return a = wanakana._romajiToKana(a, b)
 }, wanakana.toRomaji = function(a) {
     return a = wanakana._hiraganaToRomaji(a)
-}, wanakana.R_to_J = {a: "あ",i: "い",u: "う",e: "え",o: "お",yi: "い",wu: "う",whu: "う",xa: "ぁ",xi: "ぃ",xu: "ぅ",xe: "ぇ",xo: "ぉ",xyi: "ぃ",xye: "ぇ",ye: "いぇ",wha: "うぁ",whi: "うぃ",whe: "うぇ",who: "うぉ",wi: "うぃ",we: "うぇ",va: "ヴぁ",vi: "ヴぃ",vu: "ヴ",ve: "ヴぇ",vo: "ヴぉ",vya: "ヴゃ",vyi: "ヴぃ",vyu: "ヴゅ",vye: "ヴぇ",vyo: "ヴょ",ka: "か",ki: "き",ku: "く",ke: "け",ko: "こ",lka: "ヵ",lke: "ヶ",xka: "ヵ",xke: "ヶ",kya: "きゃ",kyi: "きぃ",kyu: "きゅ",kye: "きぇ",kyo: "きょ",ca: "か",ci: "き",cu: "く",ce: "け",co: "こ",lca: "ヵ",lce: "ヶ",xca: "ヵ",xce: "ヶ",qya: "くゃ",qyu: "くゅ",qyo: "くょ",qwa: "くぁ",qwi: "くぃ",qwu: "くぅ",qwe: "くぇ",qwo: "くぉ",qa: "くぁ",qi: "くぃ",qe: "くぇ",qo: "くぉ",kwa: "くぁ",qyi: "くぃ",qye: "くぇ",ga: "が",gi: "ぎ",gu: "ぐ",ge: "げ",go: "ご",gya: "ぎゃ",gyi: "ぎぃ",gyu: "ぎゅ",gye: "ぎぇ",gyo: "ぎょ",gwa: "ぐぁ",gwi: "ぐぃ",gwu: "ぐぅ",gwe: "ぐぇ",gwo: "ぐぉ",sa: "さ",si: "し",shi: "し",su: "す",se: "せ",so: "そ",za: "ざ",zi: "じ",zu: "ず",ze: "ぜ",zo: "ぞ",ji: "じ",sya: "しゃ",syi: "しぃ",syu: "しゅ",sye: "しぇ",syo: "しょ",sha: "しゃ",shu: "しゅ",she: "しぇ",sho: "しょ",shya: "しゃ",shyu: "しゅ",shye: "しぇ",shyo: "しょ",swa: "すぁ",swi: "すぃ",swu: "すぅ",swe: "すぇ",swo: "すぉ",zya: "じゃ",zyi: "じぃ",zyu: "じゅ",zye: "じぇ",zyo: "じょ",ja: "じゃ",ju: "じゅ",je: "じぇ",jo: "じょ",jya: "じゃ",jyi: "じぃ",jyu: "じゅ",jye: "じぇ",jyo: "じょ",ta: "た",ti: "ち",tu: "つ",te: "て",to: "と",chi: "ち",tsu: "つ",ltu: "っ",xtu: "っ",tya: "ちゃ",tyi: "ちぃ",tyu: "ちゅ",tye: "ちぇ",tyo: "ちょ",cha: "ちゃ",chu: "ちゅ",che: "ちぇ",cho: "ちょ",cya: "ちゃ",cyi: "ちぃ",cyu: "ちゅ",cye: "ちぇ",cyo: "ちょ",chya: "ちゃ",chyu: "ちゅ",chye: "ちぇ",chyo: "ちょ",tsa: "つぁ",tsi: "つぃ",tse: "つぇ",tso: "つぉ",tha: "てゃ",thi: "てぃ",thu: "てゅ",the: "てぇ",tho: "てょ",twa: "とぁ",twi: "とぃ",twu: "とぅ",twe: "とぇ",two: "とぉ",da: "だ",di: "ぢ",du: "づ",de: "で","do": "ど",dya: "ぢゃ",dyi: "ぢぃ",dyu: "ぢゅ",dye: "ぢぇ",dyo: "ぢょ",dha: "でゃ",dhi: "でぃ",dhu: "でゅ",dhe: "でぇ",dho: "でょ",dwa: "どぁ",dwi: "どぃ",dwu: "どぅ",dwe: "どぇ",dwo: "どぉ",na: "な",ni: "に",nu: "ぬ",ne: "ね",no: "の",nya: "にゃ",nyi: "にぃ",nyu: "にゅ",nye: "にぇ",nyo: "にょ",ha: "は",hi: "ひ",hu: "ふ",he: "へ",ho: "ほ",fu: "ふ",hya: "ひゃ",hyi: "ひぃ",hyu: "ひゅ",hye: "ひぇ",hyo: "ひょ",fya: "ふゃ",fyu: "ふゅ",fyo: "ふょ",fwa: "ふぁ",fwi: "ふぃ",fwu: "ふぅ",fwe: "ふぇ",fwo: "ふぉ",fa: "ふぁ",fi: "ふぃ",fe: "ふぇ",fo: "ふぉ",fyi: "ふぃ",fye: "ふぇ",ba: "ば",bi: "び",bu: "ぶ",be: "べ",bo: "ぼ",bya: "びゃ",byi: "びぃ",byu: "びゅ",bye: "びぇ",byo: "びょ",pa: "ぱ",pi: "ぴ",pu: "ぷ",pe: "ぺ",po: "ぽ",pya: "ぴゃ",pyi: "ぴぃ",pyu: "ぴゅ",pye: "ぴぇ",pyo: "ぴょ",ma: "ま",mi: "み",mu: "む",me: "め",mo: "も",mya: "みゃ",myi: "みぃ",myu: "みゅ",mye: "みぇ",myo: "みょ",ya: "や",yu: "ゆ",yo: "よ",xya: "ゃ",xyu: "ゅ",xyo: "ょ",ra: "ら",ri: "り",ru: "る",re: "れ",ro: "ろ",rya: "りゃ",ryi: "りぃ",ryu: "りゅ",rye: "りぇ",ryo: "りょ",la: "ら",li: "り",lu: "る",le: "れ",lo: "ろ",lya: "りゃ",lyi: "りぃ",lyu: "りゅ",lye: "りぇ",lyo: "りょ",wa: "わ",wo: "を",lwe: "ゎ",xwa: "ゎ",n: "ん",nn: "ん","n ": "ん",xn: "ん",ltsu: "っ"}, wanakana.FOUR_CHARACTER_EDGE_CASES = ["lts", "chy", "shy"], wanakana.J_to_R = {"あ": "a","い": "i","う": "u","え": "e","お": "o","ヴぁ": "va","ヴぃ": "vi","ヴ": "vu","ヴぇ": "ve","ヴぉ": "vo","か": "ka","き": "ki","きゃ": "kya","きぃ": "kyi","きゅ": "kyu","く": "ku","け": "ke","こ": "ko","が": "ga","ぎ": "gi","ぐ": "gu","げ": "ge","ご": "go","ぎゃ": "gya","ぎぃ": "gyi","ぎゅ": "gyu","ぎぇ": "gye","ぎょ": "gyo","さ": "sa","す": "su","せ": "se","そ": "so","ざ": "za","ず": "zu","ぜ": "ze","ぞ": "zo","し": "shi","しゃ": "sha","しゅ": "shu","しょ": "sho","じ": "ji","じゃ": "ja","じゅ": "ju","じょ": "jo","た": "ta","ち": "chi","ちゃ": "cha","ちゅ": "chu","ちょ": "cho","つ": "tsu","て": "te","と": "to","だ": "da","ぢ": "di","づ": "du","で": "de","ど": "do","な": "na","に": "ni","にゃ": "nya","にゅ": "nyu","にょ": "nyo","ぬ": "nu","ね": "ne","の": "no","は": "ha","ひ": "hi","ふ": "fu","へ": "he","ほ": "ho","ひゃ": "hya","ひゅ": "hyu","ひょ": "hyo","ふぁ": "fa","ふぃ": "fi","ふぇ": "fe","ふぉ": "fo","ば": "ba","び": "bi","ぶ": "bu","べ": "be","ぼ": "bo","びゃ": "bya","びゅ": "byu","びょ": "byo","ぱ": "pa","ぴ": "pi","ぷ": "pu","ぺ": "pe","ぽ": "po","ぴゃ": "pya","ぴゅ": "pyu","ぴょ": "pyo","ま": "ma","み": "mi","む": "mu","め": "me","も": "mo","みゃ": "mya","みゅ": "myu","みょ": "myo","や": "ya","ゆ": "yu","よ": "yo","ら": "ra","り": "ri","る": "ru","れ": "re","ろ": "ro","りゃ": "rya","りゅ": "ryu","りょ": "ryo","わ": "wa","を": "wo","ん": "n","ゐ": "wi","ゑ": "we","きぇ": "kye","きょ": "kyo","じぃ": "jyi","じぇ": "jye","ちぃ": "cyi","ちぇ": "che","ひぃ": "hyi","ひぇ": "hye","びぃ": "byi","びぇ": "bye","ぴぃ": "pyi","ぴぇ": "pye","みぇ": "mye","みぃ": "myi","りぃ": "ryi","りぇ": "rye","にぃ": "nyi","にぇ": "nye","しぃ": "syi","しぇ": "she","いぇ": "ye","うぁ": "wha","うぉ": "who","うぃ": "wi","うぇ": "we","ヴゃ": "vya","ヴゅ": "vyu","ヴょ": "vyo","すぁ": "swa","すぃ": "swi","すぅ": "swu","すぇ": "swe","すぉ": "swo","くゃ": "qya","くゅ": "qyu","くょ": "qyo","くぁ": "qwa","くぃ": "qwi","くぅ": "qwu","くぇ": "qwe","くぉ": "qwo","ぐぁ": "gwa","ぐぃ": "gwi","ぐぅ": "gwu","ぐぇ": "gwe","ぐぉ": "gwo","つぁ": "tsa","つぃ": "tsi","つぇ": "tse","つぉ": "tso","てゃ": "tha","てぃ": "thi","てゅ": "thu","てぇ": "the","てょ": "tho","とぁ": "twa","とぃ": "twi","とぅ": "twu","とぇ": "twe","とぉ": "two","ぢゃ": "dya","ぢぃ": "dyi","ぢゅ": "dyu","ぢぇ": "dye","ぢょ": "dyo","でゃ": "dha","でぃ": "dhi","でゅ": "dhu","でぇ": "dhe","でょ": "dho","どぁ": "dwa","どぃ": "dwi","どぅ": "dwu","どぇ": "dwe","どぉ": "dwo","ふぅ": "fwu","ふゃ": "fya","ふゅ": "fyu","ふょ": "fyo","ぁ": "a","ぃ": "i","ぇ": "e","ぅ": "u","ぉ": "o","ゃ": "ya","ゅ": "yu","ょ": "yo","っ": "","?": "ka","?": "ka","ゎ": "wa","　": " ","んあ": "n'a","んい": "n'i","んう": "n'u","んえ": "n'e","んお": "n'o","んや": "n'ya","んゆ": "n'yu","んよ": "n'yo"};
+}, wanakana.R_to_J = {a: " ",i: "¢",u: "¤",e: "¦",o: "¨",yi: "¢",wu: "¤",whu: "¤",xa: "",xi: "¡",xu: "£",xe: "¥",xo: "§",xyi: "¡",xye: "¥",ye: "¢¥",wha: "¤",whi: "¤¡",whe: "¤¥",who: "¤§",wi: "¤¡",we: "¤¥",va: "Ô",vi: "Ô¡",vu: "Ô",ve: "Ô¥",vo: "Ô§",vya: "Ôá",vyi: "Ô¡",vyu: "Ôã",vye: "Ô¥",vyo: "Ôå",ka: "©",ki: "«",ku: "­",ke: "¯",ko: "±",lka: "Õ",lke: "Ö",xka: "Õ",xke: "Ö",kya: "«á",kyi: "«¡",kyu: "«ã",kye: "«¥",kyo: "«å",ca: "©",ci: "«",cu: "­",ce: "¯",co: "±",lca: "Õ",lce: "Ö",xca: "Õ",xce: "Ö",qya: "­á",qyu: "­ã",qyo: "­å",qwa: "­",qwi: "­¡",qwu: "­£",qwe: "­¥",qwo: "­§",qa: "­",qi: "­¡",qe: "­¥",qo: "­§",kwa: "­",qyi: "­¡",qye: "­¥",ga: "ª",gi: "¬",gu: "®",ge: "°",go: "²",gya: "¬á",gyi: "¬¡",gyu: "¬ã",gye: "¬¥",gyo: "¬å",gwa: "®",gwi: "®¡",gwu: "®£",gwe: "®¥",gwo: "®§",sa: "³",si: "µ",shi: "µ",su: "·",se: "¹",so: "»",za: "´",zi: "¶",zu: "¸",ze: "º",zo: "¼",ji: "¶",sya: "µá",syi: "µ¡",syu: "µã",sye: "µ¥",syo: "µå",sha: "µá",shu: "µã",she: "µ¥",sho: "µå",shya: "µá",shyu: "µã",shye: "µ¥",shyo: "µå",swa: "·",swi: "·¡",swu: "·£",swe: "·¥",swo: "·§",zya: "¶á",zyi: "¶¡",zyu: "¶ã",zye: "¶¥",zyo: "¶å",ja: "¶á",ju: "¶ã",je: "¶¥",jo: "¶å",jya: "¶á",jyi: "¶¡",jyu: "¶ã",jye: "¶¥",jyo: "¶å",ta: "½",ti: "¿",tu: "Â",te: "Ä",to: "Æ",chi: "¿",tsu: "Â",ltu: "Á",xtu: "Á",tya: "¿á",tyi: "¿¡",tyu: "¿ã",tye: "¿¥",tyo: "¿å",cha: "¿á",chu: "¿ã",che: "¿¥",cho: "¿å",cya: "¿á",cyi: "¿¡",cyu: "¿ã",cye: "¿¥",cyo: "¿å",chya: "¿á",chyu: "¿ã",chye: "¿¥",chyo: "¿å",tsa: "Â",tsi: "Â¡",tse: "Â¥",tso: "Â§",tha: "Äá",thi: "Ä¡",thu: "Äã",the: "Ä¥",tho: "Äå",twa: "Æ",twi: "Æ¡",twu: "Æ£",twe: "Æ¥",two: "Æ§",da: "¾",di: "À",du: "Ã",de: "Å","do": "Ç",dya: "Àá",dyi: "À¡",dyu: "Àã",dye: "À¥",dyo: "Àå",dha: "Åá",dhi: "Å¡",dhu: "Åã",dhe: "Å¥",dho: "Åå",dwa: "Ç",dwi: "Ç¡",dwu: "Ç£",dwe: "Ç¥",dwo: "Ç§",na: "È",ni: "É",nu: "Ê",ne: "Ë",no: "Ì",nya: "Éá",nyi: "É¡",nyu: "Éã",nye: "É¥",nyo: "Éå",ha: "Í",hi: "Ð",hu: "Ó",he: "Ö",ho: "Ù",fu: "Ó",hya: "Ðá",hyi: "Ð¡",hyu: "Ðã",hye: "Ð¥",hyo: "Ðå",fya: "Óá",fyu: "Óã",fyo: "Óå",fwa: "Ó",fwi: "Ó¡",fwu: "Ó£",fwe: "Ó¥",fwo: "Ó§",fa: "Ó",fi: "Ó¡",fe: "Ó¥",fo: "Ó§",fyi: "Ó¡",fye: "Ó¥",ba: "Î",bi: "Ñ",bu: "Ô",be: "×",bo: "Ú",bya: "Ñá",byi: "Ñ¡",byu: "Ñã",bye: "Ñ¥",byo: "Ñå",pa: "Ï",pi: "Ò",pu: "Õ",pe: "Ø",po: "Û",pya: "Òá",pyi: "Ò¡",pyu: "Òã",pye: "Ò¥",pyo: "Òå",ma: "Ü",mi: "Ý",mu: "Þ",me: "ß",mo: "à",mya: "Ýá",myi: "Ý¡",myu: "Ýã",mye: "Ý¥",myo: "Ýå",ya: "â",yu: "ä",yo: "æ",xya: "á",xyu: "ã",xyo: "å",ra: "ç",ri: "è",ru: "é",re: "ê",ro: "ë",rya: "èá",ryi: "è¡",ryu: "èã",rye: "è¥",ryo: "èå",la: "ç",li: "è",lu: "é",le: "ê",lo: "ë",lya: "èá",lyi: "è¡",lyu: "èã",lye: "è¥",lyo: "èå",wa: "í",wo: "ð",lwe: "ì",xwa: "ì",n: "ñ",nn: "ñ","n ": "ñ",xn: "ñ",ltsu: "Á"}, wanakana.FOUR_CHARACTER_EDGE_CASES = ["lts", "chy", "shy"], wanakana.J_to_R = {" ": "a","¢": "i","¤": "u","¦": "e","¨": "o","Ô": "va","Ô¡": "vi","Ô": "vu","Ô¥": "ve","Ô§": "vo","©": "ka","«": "ki","«á": "kya","«¡": "kyi","«ã": "kyu","­": "ku","¯": "ke","±": "ko","ª": "ga","¬": "gi","®": "gu","°": "ge","²": "go","¬á": "gya","¬¡": "gyi","¬ã": "gyu","¬¥": "gye","¬å": "gyo","³": "sa","·": "su","¹": "se","»": "so","´": "za","¸": "zu","º": "ze","¼": "zo","µ": "shi","µá": "sha","µã": "shu","µå": "sho","¶": "ji","¶á": "ja","¶ã": "ju","¶å": "jo","½": "ta","¿": "chi","¿á": "cha","¿ã": "chu","¿å": "cho","Â": "tsu","Ä": "te","Æ": "to","¾": "da","À": "di","Ã": "du","Å": "de","Ç": "do","È": "na","É": "ni","Éá": "nya","Éã": "nyu","Éå": "nyo","Ê": "nu","Ë": "ne","Ì": "no","Í": "ha","Ð": "hi","Ó": "fu","Ö": "he","Ù": "ho","Ðá": "hya","Ðã": "hyu","Ðå": "hyo","Ó": "fa","Ó¡": "fi","Ó¥": "fe","Ó§": "fo","Î": "ba","Ñ": "bi","Ô": "bu","×": "be","Ú": "bo","Ñá": "bya","Ñã": "byu","Ñå": "byo","Ï": "pa","Ò": "pi","Õ": "pu","Ø": "pe","Û": "po","Òá": "pya","Òã": "pyu","Òå": "pyo","Ü": "ma","Ý": "mi","Þ": "mu","ß": "me","à": "mo","Ýá": "mya","Ýã": "myu","Ýå": "myo","â": "ya","ä": "yu","æ": "yo","ç": "ra","è": "ri","é": "ru","ê": "re","ë": "ro","èá": "rya","èã": "ryu","èå": "ryo","í": "wa","ð": "wo","ñ": "n","î": "wi","ï": "we","«¥": "kye","«å": "kyo","¶¡": "jyi","¶¥": "jye","¿¡": "cyi","¿¥": "che","Ð¡": "hyi","Ð¥": "hye","Ñ¡": "byi","Ñ¥": "bye","Ò¡": "pyi","Ò¥": "pye","Ý¥": "mye","Ý¡": "myi","è¡": "ryi","è¥": "rye","É¡": "nyi","É¥": "nye","µ¡": "syi","µ¥": "she","¢¥": "ye","¤": "wha","¤§": "who","¤¡": "wi","¤¥": "we","Ôá": "vya","Ôã": "vyu","Ôå": "vyo","·": "swa","·¡": "swi","·£": "swu","·¥": "swe","·§": "swo","­á": "qya","­ã": "qyu","­å": "qyo","­": "qwa","­¡": "qwi","­£": "qwu","­¥": "qwe","­§": "qwo","®": "gwa","®¡": "gwi","®£": "gwu","®¥": "gwe","®§": "gwo","Â": "tsa","Â¡": "tsi","Â¥": "tse","Â§": "tso","Äá": "tha","Ä¡": "thi","Äã": "thu","Ä¥": "the","Äå": "tho","Æ": "twa","Æ¡": "twi","Æ£": "twu","Æ¥": "twe","Æ§": "two","Àá": "dya","À¡": "dyi","Àã": "dyu","À¥": "dye","Àå": "dyo","Åá": "dha","Å¡": "dhi","Åã": "dhu","Å¥": "dhe","Åå": "dho","Ç": "dwa","Ç¡": "dwi","Ç£": "dwu","Ç¥": "dwe","Ç§": "dwo","Ó£": "fwu","Óá": "fya","Óã": "fyu","Óå": "fyo","": "a","¡": "i","¥": "e","£": "u","§": "o","á": "ya","ã": "yu","å": "yo","Á": "","?": "ka","?": "ka","ì": "wa","": " ","ñ ": "n'a","ñ¢": "n'i","ñ¤": "n'u","ñ¦": "n'e","ñ¨": "n'o","ñâ": "n'ya","ñä": "n'yu","ñæ": "n'yo"};
 
 function Utility() {
 }
@@ -798,13 +798,13 @@ Utility.prototype = {
             if (cur_new_word != data[i].word)
                 continue;
             txt += '<div class ="result-box">';
-            txt += '<p class="word">【' + data[i].word + '】</p>';
+            txt += '<p class="word">¹' + data[i].word + 'º</p>';
             txt += '<p class="reb">' + data[i].phonetic + '</p>';
             for (var j = 0; data[i].means != null && j < data[i].means.length; j++) {
-                txt += '<p class="field">☆' + data[i].means[j].kind + '</p>';
-                txt += '<p class="ja-mean">◆' + data[i].means[j].mean + '</p>';
+                txt += '<p class="field">Y' + data[i].means[j].kind + '</p>';
+                txt += '<p class="ja-mean">_' + data[i].means[j].mean + '</p>';
                 for (var k = 0; k < data[i].means[j].examples.length; k++) {
-                    txt += '<p class="ja-example">※' + data[i].means[j].examples[k].content + '</p>';
+                    txt += '<p class="ja-example">f' + data[i].means[j].examples[k].content + '</p>';
                     txt += '<p class="ja-trans">' + data[i].means[j].examples[k].mean + '</p>';
                 }
             }
@@ -823,7 +823,7 @@ Utility.prototype = {
                 var item = data[i];
                 if (list[r] == item.kanji) {
                     txt += '<div class ="result-box">';
-                    txt += '<p class="word">【' + item.kanji + '】</p>';
+                    txt += '<p class="word">¹' + item.kanji + 'º</p>';
                     txt += '<p class="mean"><b>Ngh?a: ' + item.mean + '</b></p><br/>';
                     txt += '<p class="comp">';
                     for (var j = 0; data[i].compDetail != null && j < data[i].compDetail.length; j++) {
@@ -838,11 +838,11 @@ Utility.prototype = {
                         txt += '<p class="level">Level: ' + item.level + '</p><br/>';
                     txt += '<p class="stroke_count">Stroke Count: ' + item.stroke_count + '</p><br/>';
                     if (item.on != null)
-                        txt += '<p class="on">Onyomi: ' + item.on.replace(/ /g, "　") + '</p><br/>';
+                        txt += '<p class="on">Onyomi: ' + item.on.replace(/ /g, "") + '</p><br/>';
                     if (item.kun != null)
-                        txt += '<p class="kun">Kunyomi: ' + item.kun.replace(/ /g, "　") + '</p><br/>';
+                        txt += '<p class="kun">Kunyomi: ' + item.kun.replace(/ /g, "") + '</p><br/>';
                     if (item.detail != null)
-                        txt += '<p class="detail">※ ' + item.detail.replace(/##/g, "<br/>※ ") + '</p></div>';
+                        txt += '<p class="detail">f ' + item.detail.replace(/##/g, "<br/>f ") + '</p></div>';
                     
                     txt += '</div>';
                     break;
@@ -863,7 +863,7 @@ Utility.prototype = {
         return t
     },
     isKanji: function(e) {
-        if (e == "々")
+        if (e == "")
             return !0;
         var t = e.charCodeAt(0);
         return t >= 19968 && t <= 40895 ? !0 : !1
@@ -1057,7 +1057,7 @@ HistoryData.prototype = {
 
 }
 
-
+TIME_LOAD_SOUND = 3;
 function AlertWord() {
     this.imageURL = "./images/logo.png";
     this.instance = null;
@@ -1087,7 +1087,7 @@ AlertWord.prototype = {
             $("#sound-remind").attr("src", soundURL);
             $("#sound-control").trigger('load');
         }
-        setTimeout(this.notifyMe.bind(this, notifTxt), 3000);
+        setTimeout(this.notifyMe.bind(this, notifTxt), TIME_LOAD_SOUND*1000);
     },
     notifyMe: function(notifTxt) {
 		if(NOTIF_FLAG == false)return;
@@ -1143,8 +1143,8 @@ ShowPanel.prototype = {
         if (!isNaN(timeShow) && timeShow > 0)
             TIME_SHOW = timeShow;
         if (!isNaN(timeDelay) && timeDelay > 0) {
-            if (timeDelay == null || timeDelay < timeShow)
-                timeDelay = timeShow;
+            if (timeDelay == null || timeDelay < timeShow+TIME_LOAD_SOUND)
+                timeDelay = timeShow+TIME_LOAD_SOUND;
             TIME_DELAY = timeDelay;
         }
         if ($('#randomChk').is(':checked')) {
